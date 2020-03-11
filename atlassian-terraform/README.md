82e71de5a7c0825cfd33018a57a6317d8123b17b
arunpatyal <arun@sumologic.com>
2020-03-11 09:10:20 +0530

    Initial Commit

---
+++ atlassian-terraform/README.md
@@ -0,0 +1,97 @@
+# sumologic-atlassian-terraform
+
+Terraform Script to deploy Sumo Logic - Atlassian Solution.
+
+## Getting Started
+
+#### Requirements
+
+* Terraform 0.12.20+
+* Required Terraform providers (Tested on mentioned versions):
+1. [Sumo Logic Terraform Provider](https://github.com/SumoLogic/sumologic-terraform-provider) "~> 2.0"
+2. [Jira Terraform Provider](https://github.com/fourplusone/terraform-provider-jira) "~> 0.1.11"
+3. [BitBucket Terraform Provider](https://github.com/terraform-providers/terraform-provider-bitbucket) "~> 1.2"
+4. [Rest API Terraform Provider](https://github.com/Mastercard/terraform-provider-restapi) "~> 1.12"
+
+#### Setup working directory
+
+1. Update placeholder values in `terraform.tfvars` to correspond to your Sumo Logic and Atlassian environments. See [list of input parameters](#configurable-parameters) below.
+2. Install required terraform providers.
+3. Initialize Terraform working directory and download plugins by running `terraform init`.
+
+#### Deploy Sumo Logic - Atlassian Solution
+
+```shell
+$ terraform plan
+$ terraform apply
+```
+#### Uninstall Sumo Logic - Atlassian Solution
+
+To uninstall the solution simply run `terraform destroy` though there are times where you may want to plan a destroy first. To do so, add the -destroy flag to terraform plan and then apply the destroy plan.
+
+## Configurable Parameters
+
+Configure below mentioned parameters in `terraform.tfvars`.
+
+### Sumo Logic
+
+[Sumo Logic Terraform Provider](https://github.com/SumoLogic/sumologic-terraform-provider)
+
+| Parameter |Description |Default Value  | URL|
+| --- | --- | --- | --- |
+| sumo_access_id            | Sumo Logic Access ID                                                  |       |https://help.sumologic.com/Manage/Security/Access-Keys|
+| sumo_access_key           | Sumo Logic Access Key                                                 |       |https://help.sumologic.com/Manage/Security/Access-Keys|
+| environment               | Sumo Logic Deployment                                                 | us1   |https://help.sumologic.com/APIs/General-API-Information/Sumo-Logic-Endpoints-and-Firewall-Security|
+| sumo_api_endpoint         | Sumo Logic API Endpoint. Make sure the trailing "/" is present.       |   https://api.sumologic.com/api/v1/    |https://help.sumologic.com/APIs/General-API-Information/Sumo-Logic-Endpoints-and-Firewall-Security|
+| app_installation_folder   | The Sumo Logic apps will be installed in a folder under your personal folder in Sumo Logic.| Atlassian|   |
+| install_jira_cloud        | Install Sumo Logic Application and WebHooks for Jira Cloud            | true  |       |
+| install_jira_on_prem      | Install Sumo Logic Application and WebHooks for Jira Server (On Prem) | true  |   https://help.sumologic.com/07Sumo-Logic-Apps/08App_Development/Jira    |
+| install_bitbucket_cloud   | Install Sumo Logic Application and WebHooks for BitBucket Cloud       | true  |       |
+| install_opsgenie          | Install Sumo Logic Application and WebHooks for OpsGenie              | true  |   https://help.sumologic.com/07Sumo-Logic-Apps/18SAAS_and_Cloud_Apps/Opsgenie    |
+| install_sumo_to_opsgenie_webhook | Install Sumo Logic to OpsGenie WebHook. Install_opsgenie should be true for this option to be true. |  true  | https://help.sumologic.com/Manage/Connections-and-Integrations/Webhook-Connections/Webhook_Connection_for_Opsgenie |
+
+## Jira Cloud
+
+[Jira Terraform Provider](https://github.com/fourplusone/terraform-provider-jira)
+
+| Parameter | Description | URL|
+| --- | --- | --- |
+| jira_cloud_url        | Jira Cloud URL                |   |
+| jira_cloud_user       | Jira Cloud Username           |   |
+| jira_cloud_password   | Jira Cloud Password or API Key           |https://confluence.atlassian.com/cloud/api-tokens-938839638.html|
+| jira_cloud_jql        | Jira Cloud Query Language Example: "project = Sumo"|https://support.atlassian.com/jira-software-cloud/docs/what-is-advanced-searching-in-jira-cloud/|
+| jira_cloud_events     | Jira Cloud Events |https://developer.atlassian.com/cloud/jira/platform/webhooks/|
+
+## Jira Server (On Prem)
+
+[Jira Terraform Provider](https://github.com/fourplusone/terraform-provider-jira)
+
+| Parameter | Description | URL|
+| --- | --- | --- |
+| jira_on_prem_url        | Jira Server URL                |   |
+| jira_on_prem_user       | Jira Server Username           |   |
+| jira_on_prem_password   | Needs to be the password. API Key is not supported on Jira Server yet.           |  |
+| jira_on_prem_jql        | Jira Server Query Language Example: "project = Sumo"|https://support.atlassian.com/jira-software-cloud/docs/what-is-advanced-searching-in-jira-cloud/|
+| jira_on_prem_events     | Jira Server Events |https://developer.atlassian.com/server/jira/platform/webhooks/|
+
+## BitBucket
+
+[BitBucket Terraform Provider](https://github.com/terraform-providers/terraform-provider-bitbucket)
+
+| Parameter | Description | URL|
+| --- | --- | --- |
+| bitbucket_cloud_user          | BitBucket Username           |   |
+| bitbucket_cloud_password      | BitBucket password or App Password          | https://confluence.atlassian.com/bitbucket/app-passwords-828781300.html |
+| bitbucket_cloud_owner         | The owner of the repositories. Can be you or any team you have write access to|       |
+| bitbucket_cloud_repos         | Specify the repositories for which WebHooks should be created. Format: ["repo1","repo2"]           |   |
+| bitbucket_cloud_desc          | The name / description to show in the UI          |  |
+| bitbucket_cloud_events        | BitBucket Events to track           |https://confluence.atlassian.com/bitbucket/event-payloads-740262817.html|
+
+## OpsGenie
+
+[Rest API Terraform Provider](https://github.com/Mastercard/terraform-provider-restapi)
+
+| Parameter | Description | URL|
+| --- | --- | --- |
+| opsgenie_api_url        | OpsGenie API URL. Do not add the trailing "/". If using the EU instance of Opsgenie, the URL needs to be https://api.eu.opsgenie.com for requests to be executed.                 | https://docs.opsgenie.com/docs/api-overview
+| opsgenie_key            | OpsGenie API Key            | https://docs.opsgenie.com/docs/api-integration
---
+++ atlassian-terraform/sumologic.tf
@@ -0,0 +1,156 @@
+# This script creates Sumo Logic Collector and Sources for Atlassian Systems.
+# Configure the Sumo Logic credentials in the terraform.tvars.
+
+# Sumologic Provider
+provider "sumologic" {
+access_id = "${var.sumo_access_id}"
+access_key = "${var.sumo_access_key}"
+environment = "${var.environment}"
+}
+
+# Create/Delete Collector
+resource "sumologic_collector" "atlassian_collector" {
+name = "Atlassian Collector"
+category = "Atlassian"
+}
+
+# Create/Delete Jira Cloud Source
+resource "sumologic_http_source" "jira_cloud" {
+    count = "${var.install_jira_cloud}" ? 1:0
+    name = "Jira Cloud"
+    category = "Atlassian/Jira/Cloud"
+    collector_id = "${sumologic_collector.atlassian_collector.id}"
+}
+
+# Create/Delete BitBucket Cloud Source
+resource "sumologic_http_source" "bitbucket_cloud" {
+    count = "${var.install_bitbucket_cloud}" ? 1:0
+    name = "Bitbucket Cloud"
+    category = "Atlassian/Bitbucket"
+    collector_id = "${sumologic_collector.atlassian_collector.id}"
+}
+
+# Create/Delete Jira On Prem Source
+resource "sumologic_http_source" "jira_on_prem" {
+    count = "${var.install_jira_on_prem}" ? 1:0
+    name = "Jira On-prem"
+    category = "Atlassian/Jira/Server"
+    collector_id = "${sumologic_collector.atlassian_collector.id}"
+}
+
+# Create/Delete OpsGenie Source
+resource "sumologic_http_source" "opsgenie" {
+    count = "${var.install_opsgenie}" ? 1:0
+    name = "OpsGenie"
+    category = "Atlassian/OpsGenie"
+    collector_id = "${sumologic_collector.atlassian_collector.id}"
+}
+
+# Sumo Logic - Create/Delete folder for installing the applications
+data "sumologic_personal_folder" "personalFolder" {}
+
+resource "sumologic_folder" "folder" {
+  name        = "${var.app_installation_folder}"
+  description = "Atlassian Applications"
+  parent_id   = "${data.sumologic_personal_folder.personalFolder.id}"
+  depends_on  = [sumologic_collector.atlassian_collector]
+}
+
+# Install Apps
+# Install Jira Cloud - Needs UUID and Desc change
+resource "null_resource" "install_jira_cloud_app" {
+ count = "${var.install_jira_cloud}" ? 1:0
+ depends_on = [sumologic_http_source.jira_cloud]
+
+ provisioner "local-exec" {
+     command = <<EOT
+     curl -s --request POST '${var.sumo_api_endpoint}apps/ec905b32-e514-4c63-baac-0efa3b3a2109/install' \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' \
+        -u "${var.sumo_access_id}:${var.sumo_access_key}" \
+        --data-raw '{ "name": "Jira Cloud", "description": "The Sumo Logic App for Jira Cloud provides insight into Jira usage, request activity, issues, security, sprint events, and user events.", "destinationFolderId": "${sumologic_folder.folder.id}","dataSourceValues": {"jiralogsrc": "_sourceCategory = Atlassian/Jira/Cloud" }}'
+EOT
+ }
+}
+
+# Install Jira Onprem
+resource "null_resource" "install_jira_on_prem_app" {
+ count = "${var.install_jira_on_prem}" ? 1:0
+ depends_on = [sumologic_http_source.jira_on_prem]
+
+ provisioner "local-exec" {
+     command = <<EOT
+     curl -s --request POST '${var.sumo_api_endpoint}apps/ec905b32-e514-4c63-baac-0efa3b3a2109/install' \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' \
+        -u "${var.sumo_access_id}:${var.sumo_access_key}" \
+        --data-raw '{ "name": "Jira", "description": "The Sumo Logic App for Jira provides insight into Jira usage, request activity, issues, security, sprint events, and user events.", "destinationFolderId": "${sumologic_folder.folder.id}","dataSourceValues": {"jiralogsrc": "_sourceCategory = Atlassian/Jira/Server" }}'
+EOT
+ }
+}
+
+# Install Bitbucket - Pending UUID and Desc change
+resource "null_resource" "install_bitbucket_cloud_app" {
+ count = "${var.install_bitbucket_cloud}" ? 1:0
+ depends_on = [sumologic_http_source.bitbucket_cloud]
+
+ provisioner "local-exec" {
+     command = <<EOT
+     curl -s --request POST '${var.sumo_api_endpoint}apps/ec905b32-e514-4c63-baac-0efa3b3a2109/install' \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' \
+        -u "${var.sumo_access_id}:${var.sumo_access_key}" \
+        --data-raw '{ "name": "Bitbucket", "description": "The Sumo Logic App for Jira provides insight into Jira usage, request activity, issues, security, sprint events, and user events.", "destinationFolderId": "${sumologic_folder.folder.id}","dataSourceValues": {"jiralogsrc": "_sourceCategory = Atlassian/Bitbucket" }}'
+EOT
+ }
+}
+
+# Install Opsgenie
+resource "null_resource" "install_Opsgenie_app" {
+ count = "${var.install_opsgenie}" ? 1:0
+ depends_on = [sumologic_http_source.opsgenie]
+
+ provisioner "local-exec" {
+     command = <<EOT
+     curl -s --request POST '${var.sumo_api_endpoint}apps/05bf2074-0487-486f-8359-3d878fbc1c49/install' \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' \
+        -u "${var.sumo_access_id}:${var.sumo_access_key}" \
+        --data-raw '{ "name": "Opsgenie", "description": "The Opsgenie App provides at-a-glance views and detailed analytics for alerts on your DevOps environment.", "destinationFolderId": "${sumologic_folder.folder.id}","dataSourceValues": {"opsgenieLogSrc": "_sourceCategory = Atlassian/OpsGenie" }}'
+EOT
+ }
+}
+
+# Create/Delete Sumo Logic to OpsGenie Webhook i.e. Connection in Sumo Logic by calling REST API
+# https://help.sumologic.com/Manage/Connections-and-Integrations/Webhook-Connections/Webhook_Connection_for_Opsgenie
+provider "restapi" {
+  alias = "sumo"
+  uri                  = "${var.sumo_api_endpoint}"
+# debug                = true
+  write_returns_object = true
+  username = "${var.sumo_access_id}"
+  password = "${var.sumo_access_key}"
+  headers = {Content-Type = "application/json"}
+}
+
+locals {
+  value = jsondecode(restapi_object.ops_to_sumo_webhook[0].create_response).data.apiKey
+}
+
+data "template_file" "data_json_sto" {
+  count = "${var.install_sumo_to_opsgenie_webhook}" ? 1:0
+  template = "${file("${path.module}/sumo_to_opsgenie_webhook.json.tmpl")}"
+  vars = {
+    url = "${var.opsgenie_api_url}/v1/json/sumologic?apiKey=${local.value}"
+  }
+}
+
+resource "restapi_object" "sumo_to_opsgenie_webhook" {
+  provider = restapi.sumo
+  count = "${var.install_sumo_to_opsgenie_webhook}" ?1:0
+  path = "/connections"
+  destroy_path = "/connections/{id}?type=WebhookConnection"
+# debug = true
+  id_attribute = "id"
+  data = "${data.template_file.data_json_sto[0].rendered}"
+}
