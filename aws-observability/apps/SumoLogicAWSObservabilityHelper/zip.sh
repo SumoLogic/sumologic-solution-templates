c23ee2ba455adbf879812aa5b920f46f305e964c
sourabh <sourabh@sumologic.com>
2020-05-27 18:36:41 +0530

    adding new Zip file.

--- aws-observability/apps/SumoLogicAWSObservabilityHelper/zip.sh
+++ aws-observability/apps/SumoLogicAWSObservabilityHelper/zip.sh
@@ -2,15 +2,8 @@ if [ -f SumoLogicAWSObservabilityHelper.zip ]; then
     rm SumoLogicAWSObservabilityHelper.zip
 fi
 
-if [ ! -f SumoLogicAWSObservabilityHelper.zip ]; then
-    echo "creating zip file"
-    mkdir python
-    cd python
-    pip install  crhelper -t .
-    pip install requests -t .
-    pip install retrying -t .
-    cp -v ../../../sumologic-app-utils/src/*.py .
-    zip -r ../SumoLogicAWSObservabilityHelper.zip .
-    cd ..
-    rm -r python
-fi
+export AWS_PROFILE="default"
+
+export version="v2.0.0"
+
+aws s3 cp s3://appdevstore/sumo_app_utils/${version}/sumo_app_utils.zip SumoLogicAWSObservabilityHelper.zip --profile ${AWS_PROFILE} --region us-east-1
