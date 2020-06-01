b0424a724716290276951f4340a143502da01233
sourabh <sourabh@sumologic.com>
2020-06-01 12:16:40 +0530

    Adding Sleep after each Resource creation to handle API limit error.

--- aws-observability/apps/SumoLogicAWSObservabilityHelper/zip.sh
+++ aws-observability/apps/SumoLogicAWSObservabilityHelper/zip.sh
@@ -1,9 +1,15 @@
-if [ -f SumoLogicAWSObservabilityHelper.zip ]; then
+#!/usr/bin/env bash
+
+export version="v2.0.1"
+
+if [[ -f SumoLogicAWSObservabilityHelper.zip ]]; then
     rm SumoLogicAWSObservabilityHelper.zip
 fi
 
-export AWS_PROFILE="default"
+if [[ -f SumoLogicAWSObservabilityHelper${version}.zip ]]; then
+    rm SumoLogicAWSObservabilityHelper${version}.zip
+fi
 
-export version="v2.0.0"
+export AWS_PROFILE="personal"
 
-aws s3 cp s3://appdevstore/sumo_app_utils/${version}/sumo_app_utils.zip SumoLogicAWSObservabilityHelper.zip --profile ${AWS_PROFILE} --region us-east-1
+aws s3 cp s3://sumologiclambdahelper-us-east-1/sumo_app_utils/${version}/sumo_app_utils.zip SumoLogicAWShObservabilityHelper${version}.zip --profile ${AWS_PROFILE} --region us-east-1
