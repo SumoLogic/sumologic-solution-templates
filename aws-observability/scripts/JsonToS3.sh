aa59d1fca88543c42e36ea07f932b9cc3f555b1b
sourabh <sourabh@sumologic.com>
2020-06-16 11:42:14 +0530

    adding new app JSON, correcting Description in Master Template. Fail safe in scripts to not upload templates by accident.

--- aws-observability/scripts/JsonToS3.sh
+++ aws-observability/scripts/JsonToS3.sh
@@ -2,6 +2,8 @@
 
 echo "Start S3 upload Script....."
 
+export AWS_PROFILE="prod"
+
 bucket_name=app-json-store
 match_case="AWS"
 
@@ -9,8 +11,11 @@ yourfilenames=`ls ../json/*.json`
 for app_file in ${yourfilenames}
 do
 	if [[ "${app_file}" == *"${match_case}"* ]]; then
-		echo "File Name is "${app_file}
-    	aws s3 cp ${app_file} s3://${bucket_name}/ --acl public-read
+		if [[ ${AWS_PROFILE} == 'default' ]]
+		then
+    	    aws s3 cp ${app_file} s3://${bucket_name}/ --acl public-read --profile ${AWS_PROFILE}
+    	    echo "Uploaded File Name -> ${app_file} to bucket -> ${bucket_name}"
+    	fi
     fi
 done
 
--- aws-observability/scripts/YamlToS3.sh
+++ aws-observability/scripts/YamlToS3.sh
@@ -2,6 +2,8 @@
 
 echo "Start S3 upload Script....."
 
+export AWS_PROFILE="prod"
+
 declare -a regions=("us-east-2" "us-east-1" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1s" "sa-east-1" "ap-east-1s" "af-south-1s" "eu-south-1" "me-south-1s")
 
 cd ..\/
@@ -14,12 +16,19 @@ do
     then
         export region=`echo "${region%?}"`
     fi
-    echo "Region is $region and Bucket Name is $bucket_name"
-    aws s3 cp . s3://$bucket_name/sumologic-aws-observability/ --region ${region} --recursive --exclude '*.sh' --exclude '*.json' --exclude '.git/*' --exclude '.idea/*' --acl public-read
+
+    aws s3 cp . s3://$bucket_name/sumologic-aws-observability/ --region ${region} --recursive --exclude '*.sh' --exclude '*.json' --exclude '.git/*' --exclude '.idea/*' --acl public-read --profile ${AWS_PROFILE}
+
+    echo "Upload complete for Region -> $region and Bucket Name -> $bucket_name"
 done
 
 cd templates/
 
-aws s3 cp sumologic_observability.master.template.yaml s3://sumologic-appdev-aws-sam-apps/ --acl public-read
+if [[ ${AWS_PROFILE} == 'default' ]]
+then
+    aws s3 cp sumologic_observability.master.template.yaml s3://sumologic-appdev-aws-sam-apps/ --acl public-read --profile ${AWS_PROFILE}
+
+    echo "Upload complete for Master Template to Bucket Name -> sumologic-appdev-aws-sam-apps"
+fi
 
 echo "End S3 upload Script....."
