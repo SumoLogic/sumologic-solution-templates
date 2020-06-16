91c652708a5b734c0ac0c61772314afebf25bf43
sourabh <sourabh@sumologic.com>
2020-06-16 12:03:17 +0530

    adding sh file to validate template for linting and security concerns.

---
+++ aws-observability/scripts/ValidateTemplates.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+declare -a apps=('alb' 'anomaly' 'apigateway' 'AutoEnableS3LogsAlb' 'AutoTagAWSResources' 'common' 'dynamodb' 'ec2metrics' 'lambda' 'rds' 'master')
+
+cd ..\/
+
+for app in "${apps[@]}"
+do
+
+    if [[ "${app}" != "master" ]]
+    then
+        path=apps/${app}
+    else
+        path=templates
+    fi
+    output=`cfn-lint ./${path}/*.yaml`
+    echo "Validation complete for File -> app with Output as ${output}"
+
+    output=`cfn_nag ./${path}/*.yaml`
+    echo "Security Validation complete for File -> app with Output as \n ${output}"
+done
