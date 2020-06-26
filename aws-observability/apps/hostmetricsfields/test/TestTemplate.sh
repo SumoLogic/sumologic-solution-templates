7142be62485c0c35c8f409a957e8571d63f750b9
sourabh <sourabh@sumologic.com>
2020-06-26 14:10:49 +0530

    adding region list primerfunction in case of update

--- aws-observability/apps/hostmetricsfields/test/TestTemplate.sh
+++ aws-observability/apps/hostmetricsfields/test/TestTemplate.sh
@@ -12,8 +12,8 @@ export Section1bSumoAccessID=$6
 export Section1cSumoAccessKey=$7
 export Section1dRemoveSumoResourcesOnDeleteStack=true
 
-export Section2aAccountAlias=${InstallType}
-export Section2bRegionList="us-east-1, us-east-2, us-west-1, us-west-2, ap-south-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, ap-northeast-1, ca-central-1, eu-central-1, eu-west-1, eu-west-2, eu-west-3, eu-north-1, sa-east-1, ap-east-1"
+export Section2aAccountAlias="${InstallType}"
+export Section2bRegionList="eu-north-1, sa-east-1, ap-east-1"
 export Section2cUpdateVersion=2
 
 
