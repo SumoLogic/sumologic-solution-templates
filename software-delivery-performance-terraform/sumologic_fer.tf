1d70fdd7f6daa25adbddef69f5c9fc27839d0a79
arunpatyal <arun@sumologic.com>
2020-08-06 13:54:23 +0530

    SDO App Json Import Changes.

--- software-delivery-performance-terraform/sumologic_fer.tf
+++ software-delivery-performance-terraform/sumologic_fer.tf
@@ -3,7 +3,7 @@
 resource "sumologic_field_extraction_rule" "github_pr_fer" {
   count            = "${var.install_github}" ? 1 : 0
   depends_on       = [sumologic_http_source.github]
-  name             = "Github Pull Request0"
+  name             = "Github Pull Request"
   scope            = "_sourceCategory=${var.github_sc} ${var.github_pull_request_fer_scope}"
   parse_expression = var.github_pull_request_fer_parse
   enabled          = true
@@ -12,7 +12,7 @@ resource "sumologic_field_extraction_rule" "github_pr_fer" {
 resource "sumologic_field_extraction_rule" "jenkins_build_fer" {
   count            = "${var.install_jenkins}" ? 1 : 0
   depends_on       = [sumologic_http_source.jenkins]
-  name             = "Jenkins Build0"
+  name             = "Jenkins Build"
   scope            = "_sourceCategory=${var.jenkins_sc} ${var.jenkins_build_fer_scope}"
   parse_expression = var.jenkins_build_fer_parse
   enabled          = true
@@ -21,7 +21,7 @@ resource "sumologic_field_extraction_rule" "jenkins_build_fer" {
 resource "sumologic_field_extraction_rule" "jenkins_deploy_fer" {
   count            = "${var.install_jenkins}" ? 1 : 0
   depends_on       = [sumologic_http_source.jenkins]
-  name             = "Jenkins Deploy0"
+  name             = "Jenkins Deploy"
   scope            = "_sourceCategory=${var.jenkins_sc} ${var.jenkins_deploy_fer_scope}"
   parse_expression = var.jenkins_deploy_fer_parse
   enabled          = true
@@ -30,7 +30,7 @@ resource "sumologic_field_extraction_rule" "jenkins_deploy_fer" {
 resource "sumologic_field_extraction_rule" "opsgenie_alerts_fer" {
   count            = "${var.install_opsgenie}" ? 1 : 0
   depends_on       = [sumologic_http_source.opsgenie]
-  name             = "Opsgenie Alerts0"
+  name             = "Opsgenie Alerts"
   scope            = "_sourceCategory=${var.opsgenie_sc} ${var.opsgenie_alerts_fer_scope}"
   parse_expression = var.opsgenie_alerts_fer_parse
   enabled          = true
@@ -39,7 +39,7 @@ resource "sumologic_field_extraction_rule" "opsgenie_alerts_fer" {
 resource "sumologic_field_extraction_rule" "bitbucket_pr_fer" {
   count            = "${var.install_bitbucket_cloud}" ? 1 : 0
   depends_on       = [sumologic_http_source.bitbucket_cloud]
-  name             = "Bitbucket Pull Request0"
+  name             = "Bitbucket Pull Request"
   scope            = "_sourceCategory=${var.bitbucket_sc} ${var.bitbucket_pull_request_fer_scope}"
   parse_expression = var.bitbucket_pull_request_fer_parse
   enabled          = true
@@ -48,7 +48,7 @@ resource "sumologic_field_extraction_rule" "bitbucket_pr_fer" {
 resource "sumologic_field_extraction_rule" "bitbucket_build_fer" {
   count            = "${var.install_bitbucket_cloud}" ? 1 : 0
   depends_on       = [sumologic_http_source.bitbucket_cloud]
-  name             = "Bitbucket Build1"
+  name             = "Bitbucket Build"
   scope            = "_sourceCategory=${var.bitbucket_sc} ${var.bitbucket_build_fer_scope}"
   parse_expression = var.bitbucket_build_fer_parse
   enabled          = true
@@ -57,7 +57,7 @@ resource "sumologic_field_extraction_rule" "bitbucket_build_fer" {
 resource "sumologic_field_extraction_rule" "bitbucket_deploy_fer" {
   count            = "${var.install_bitbucket_cloud}" ? 1 : 0
   depends_on       = [sumologic_http_source.bitbucket_cloud]
-  name             = "Bitbucket Deploy0"
+  name             = "Bitbucket Deploy"
   scope            = "_sourceCategory=${var.bitbucket_sc} ${var.bitbucket_deploy_fer_scope}"
   parse_expression = var.bitbucket_deploy_fer_parse
   enabled          = true
@@ -66,7 +66,7 @@ resource "sumologic_field_extraction_rule" "bitbucket_deploy_fer" {
 resource "sumologic_field_extraction_rule" "jira_cloud_issues_fer" {
   count            = "${var.install_jira_cloud}" ? 1 : 0
   depends_on       = [sumologic_http_source.jira_cloud]
-  name             = "Jira Cloud Issues0"
+  name             = "Jira Cloud Issues"
   scope            = "_sourceCategory=${var.jira_cloud_sc} ${var.jira_cloud_issues_fer_scope}"
   parse_expression = var.jira_cloud_issues_fer_parse
   enabled          = true
@@ -75,7 +75,7 @@ resource "sumologic_field_extraction_rule" "jira_cloud_issues_fer" {
 resource "sumologic_field_extraction_rule" "jira_server_issues_fer" {
   count            = "${var.install_jira_server}" ? 1 : 0
   depends_on       = [sumologic_http_source.jira_server]
-  name             = "Jira Server Issues0"
+  name             = "Jira Server Issues"
   scope            = "_sourceCategory=${var.jira_server_sc} ${var.jira_server_issues_fer_scope}"
   parse_expression = var.jira_server_issues_fer_parse
   enabled          = true
@@ -84,7 +84,7 @@ resource "sumologic_field_extraction_rule" "jira_server_issues_fer" {
 resource "sumologic_field_extraction_rule" "pagerduty_alerts_fer" {
   count            = "${var.install_pagerduty}" ? 1 : 0
   depends_on       = [sumologic_http_source.pagerduty]
-  name             = "Pagerduty Alerts0"
+  name             = "Pagerduty Alerts"
   scope            = "_sourceCategory=${var.pagerduty_sc} ${var.pagerduty_alerts_fer_scope}"
   parse_expression = var.pagerduty_alerts_fer_parse
   enabled          = true
