8896b005999adf88824f01cf03cfd9b6b9b16a35
sourabh <sourabh@sumologic.com>
2020-12-29 13:03:12 +0530

    adding condition to upload zip

--- aws-observability/scripts/YamlToS3.sh
+++ aws-observability/scripts/YamlToS3.sh
@@ -9,19 +9,22 @@ declare -a regions=("us-east-2" "us-east-1" "us-west-1" "us-west-2" "ap-south-1"
 cd ..\/
 
 # Upload the ZIP file to bucket appdevzipfiles- in every region with the new version.
-for region in "${regions[@]}"
-do
-    bucket_name=appdevzipfiles-$region
+if [[ ${AWS_PROFILE} == 'default' ]]
+then
+  for region in "${regions[@]}"
+  do
+      bucket_name=appdevzipfiles-$region
 
-    if [[ `echo ${region} | awk '{print substr($0,length,1)}'` == "s" ]]
-    then
-        export region=`echo "${region%?}"`
-    fi
+      if [[ `echo ${region} | awk '{print substr($0,length,1)}'` == "s" ]]
+      then
+          export region=`echo "${region%?}"`
+      fi
 
-    aws s3 cp apps/SumoLogicAWSObservabilityHelper/ s3://${bucket_name}/sumologic-aws-observability/apps/SumoLogicAWSObservabilityHelper/ --recursive --include '*.zip' --exclude '*.sh' --region ${region} --acl public-read --profile ${AWS_PROFILE}
+      aws s3 cp apps/SumoLogicAWSObservabilityHelper/ s3://${bucket_name}/sumologic-aws-observability/apps/SumoLogicAWSObservabilityHelper/ --recursive --include '*.zip' --exclude '*.sh' --region ${region} --acl public-read --profile ${AWS_PROFILE}
 
-    echo "ZIP Upload complete for Region -> ${region} and Bucket Name -> ${bucket_name}"
-done
+      echo "ZIP Upload complete for Region -> ${region} and Bucket Name -> ${bucket_name}"
+  done
+fi
 
 # Upload Control Tower and Permission Check template to sumologic-appdev-aws-sam-apps bucket
 export bucket_name=sumologic-appdev-aws-sam-apps
@@ -40,7 +43,7 @@ if [[ ${AWS_PROFILE} == 'default' ]]
 then
     export version=2.0.1
 
-    aws s3 cp apps/ s3://${bucket_name}/aws-observability-versions/${version}/ --recursive --include "*.template.yaml" --exclude '*.zip' --exclude '*.sh' --exclude 'apps/*/test/*' --exclude '*/test/*' --region ${region} --acl public-read --profile ${AWS_PROFILE}
+    aws s3 cp apps/ s3://${bucket_name}/aws-observability-versions/${version}/ --recursive --include "*.template.yaml" --exclude '*.zip' --exclude '*.sh' --exclude 'apps/*/test/*' --exclude '*/test/*' --acl public-read --profile ${AWS_PROFILE}
 
     aws s3 cp templates/sumologic_observability.master.template.yaml s3://${bucket_name}/aws-observability-versions/${version}/ --acl public-read --profile ${AWS_PROFILE}
 
