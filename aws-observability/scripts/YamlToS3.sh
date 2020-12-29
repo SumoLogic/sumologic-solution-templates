160f05a551a8bee07d9d1894f3454b400565e847
sourabh <sourabh@sumologic.com>
2020-12-29 12:54:44 +0530

    Updating sh to upload ZIP to all regions and template to single bucket.

--- aws-observability/scripts/YamlToS3.sh
+++ aws-observability/scripts/YamlToS3.sh
@@ -8,6 +8,7 @@ declare -a regions=("us-east-2" "us-east-1" "us-west-1" "us-west-2" "ap-south-1"
 
 cd ..\/
 
+# Upload the ZIP file to bucket appdevzipfiles- in every region with the new version.
 for region in "${regions[@]}"
 do
     bucket_name=appdevzipfiles-$region
@@ -17,32 +18,33 @@ do
         export region=`echo "${region%?}"`
     fi
 
-    aws s3 cp . s3://$bucket_name/sumologic-aws-observability/ --region ${region} --recursive --exclude '*.sh' --exclude 'apps/*/test/*' --exclude '*/test/*' --exclude '*.json' --exclude '.git/*' --exclude '.idea/*' --acl public-read --profile ${AWS_PROFILE}
+    aws s3 cp apps/SumoLogicAWSObservabilityHelper/ s3://${bucket_name}/sumologic-aws-observability/apps/SumoLogicAWSObservabilityHelper/ --recursive --include '*.zip' --exclude '*.sh' --region ${region} --acl public-read --profile ${AWS_PROFILE}
 
-    echo "Upload complete for Region -> $region and Bucket Name -> $bucket_name"
+    echo "ZIP Upload complete for Region -> ${region} and Bucket Name -> ${bucket_name}"
 done
 
-if [[ ${AWS_PROFILE} == 'default' ]]
-then
-    aws s3 cp apps/permissionchecker/permissioncheck.template.yaml s3://sumologic-appdev-aws-sam-apps/ --acl public-read --profile ${AWS_PROFILE}
+# Upload Control Tower and Permission Check template to sumologic-appdev-aws-sam-apps bucket
+export bucket_name=sumologic-appdev-aws-sam-apps
 
-    echo "Upload complete for Permission check Template to Bucket Name -> sumologic-appdev-aws-sam-apps"
+if [[ ${AWS_PROFILE} == 'personal' ]]
+then
+    aws s3 cp apps/permissionchecker/permissioncheck.template.yaml s3://${bucket_name}/ --acl public-read --profile ${AWS_PROFILE}
+    echo "Upload complete for Permission check Template to Bucket Name -> ${bucket_name}"
+
+    aws s3 cp apps/controltower/controltower.template.yaml s3://${bucket_name}/ --acl public-read --profile ${AWS_PROFILE}
+    echo "Upload complete for Control Tower Template to Bucket Name -> ${bucket_name}"
 fi
 
-if [[ ${AWS_PROFILE} == 'default' ]]
+# Upload all templates to sumologic-appdev-aws-sam-apps bucket with version information.
+if [[ ${AWS_PROFILE} == 'personal' ]]
 then
-    aws s3 cp apps/controltower/controltower.template.yaml s3://sumologic-appdev-aws-sam-apps/ --acl public-read --profile ${AWS_PROFILE}
+    export version=2.0.1
 
-    echo "Upload complete for Control Tower Template to Bucket Name -> sumologic-appdev-aws-sam-apps"
-fi
+    aws s3 cp apps/ s3://${bucket_name}/aws-observability-versions/${version}/ --recursive --include "*.template.yaml" --exclude '*.zip' --exclude '*.sh' --exclude 'apps/*/test/*' --exclude '*/test/*' --region ${region} --acl public-read --profile ${AWS_PROFILE}
 
-cd templates/
+    aws s3 cp templates/sumologic_observability.master.template.yaml s3://${bucket_name}/aws-observability-versions/${version}/ --acl public-read --profile ${AWS_PROFILE}
 
-if [[ ${AWS_PROFILE} == 'default' ]]
-then
-    aws s3 cp sumologic_observability.master.template.yaml s3://sumologic-appdev-aws-sam-apps/ --acl public-read --profile ${AWS_PROFILE}
-
-    echo "Upload complete for Master Template to Bucket Name -> sumologic-appdev-aws-sam-apps"
+    echo "Upload complete for Master and Nested Template to Bucket Name -> ${bucket_name}"
 fi
 
 echo "End S3 upload Script....."
