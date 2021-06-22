8bf033418c08f696a8e9188d45758b1cf3793d98
Nitin Pande <npande@sumologic.com>
2021-06-22 18:19:42 +0530

    Removed redundant script code from app module.

--- aws-observability-terraform/app-modules/common/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  api_endpoint = var.environment == "us1" ? "https://api.sumologic.com/api" : "https://api.${var.environment}.sumologic.com/api"
-
-  folder_path = dirname(dirname(path.cwd))
-}
