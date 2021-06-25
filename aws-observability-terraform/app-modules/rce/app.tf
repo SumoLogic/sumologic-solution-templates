84ce9fbe2763345319c0710f0959d4f3af5fec8b
Nitin Pande <npande@sumologic.com>
2021-06-25 20:19:47 +0530

    Updated rce app module to install GIS for CloudTrail DevOps app which is part of sumo logic app catalog.

--- aws-observability-terraform/app-modules/rce/app.tf
+++ aws-observability-terraform/app-modules/rce/app.tf
@@ -21,3 +21,35 @@ module "rce_module" {
 
   # ********************** Monitors ********************** #
 }
+
+# ********************** Install App from App Catalog ********************** #
+locals {
+  api_endpoint    = var.environment == "us1" ? "https://api.sumologic.com/api" : "https://api.${var.environment}.sumologic.com/api"
+  app_name        = "Global Intelligence for CloudTrail DevOps"
+  app_description = "Global Intelligence for AWS CloudTrail - DevOps helps infrastructure engineers configure managed AWS services for outage-resistance and on-call staff to accelerate RCA for incidents by providing insights sourced from error rates and config practices."
+  app_id          = "c7e195de-f169-460a-8e8b-7bb23af0ee5e"
+  app_datasource  = jsonencode({"CloudTrailLogSrc": "account=* eventSource"})
+}
+resource "null_resource" "SumoLogicCatalogApps" {
+  triggers = {
+    app_name        = local.app_name
+    app_description = local.app_description
+    app_id          = local.app_id
+    app_datasource  = local.app_datasource
+    access_id       = var.access_id
+    access_key      = var.access_key
+    api_endpoint    = local.api_endpoint
+    folder_id       = var.app_folder_id
+  }
+
+  provisioner "local-exec" {
+    when    = create
+    command = <<EOT
+        curl -s --request POST '${local.api_endpoint}/v1/apps/${local.app_id}/install' \
+            --header 'Accept: application/json' \
+            --header 'Content-Type: application/json' \
+            -u ${var.access_id}:${var.access_key} \
+            --data-raw '{"name": "${self.triggers.app_name}", "description": "${self.triggers.app_description}", "destinationFolderId": "${var.app_folder_id}", "dataSourceValues": ${self.triggers.app_datasource}}'
+    EOT
+  }
+}
