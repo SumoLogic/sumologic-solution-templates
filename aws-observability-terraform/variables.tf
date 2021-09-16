10ccf642f0ba324e3131769f9b4fa2ea227e099e
Nitin Pande <npande@sumologic.com>
2021-09-16 18:44:33 +0530

    added sensitive flag as it was giving problem with Tf v 1.0.x

--- aws-observability-terraform/variables.tf
+++ aws-observability-terraform/variables.tf
@@ -30,6 +30,7 @@ variable "sumologic_access_id" {
 variable "sumologic_access_key" {
   type        = string
   description = "Sumo Logic Access Key. Visit https://help.sumologic.com/Manage/Security/Access-Keys#Create_an_access_key"
+  sensitive = true
 
   validation {
     condition     = can(regex("\\w+", var.sumologic_access_key))
