582193d8a4f73ef159aa72ba75c70cca938a963d
Nitin Pande <npande@sumologic.com>
2021-09-16 18:44:33 +0530

    added sensitive flag as it was giving problem with Tf v 1.0.x

--- aws-observability-terraform/output.tf
+++ aws-observability-terraform/output.tf
@@ -1,9 +1,11 @@
 output "Apps" {
   value       = module.sumo-module
   description = "All outputs related to apps."
+  sensitive = true
 }
 
 output "Collection" {
   value       = module.collection-module
   description = "All outputs related to collection and sources."
+  sensitive = true
 }
