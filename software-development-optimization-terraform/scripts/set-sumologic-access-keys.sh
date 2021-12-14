c5882e740fd461eed78b1e20a554c954d16b8913
Carl Caum <carl@carlcaum.com>
2021-12-14 10:42:31 -0800

    Support setting the Sumo API endpoint

--- software-development-optimization-terraform/scripts/set-sumologic-access-keys.sh
+++ software-development-optimization-terraform/scripts/set-sumologic-access-keys.sh
@@ -15,3 +15,43 @@ echo -n "Sumo Logic Access Key: "
 sumologic_access_key=""
 read -r sumologic_access_key
 ./scripts/update_sdo_variable sumo_access_key $sumologic_access_key
+
+echo -n "Sumo Logic Deployment Region: "
+sumologic_deployment_region=""
+read -r sumologic_deployment_region
+./scripts/update_sdo_variable deployment $sumologic_deployment_region
+
+# Make sure we only have to deal with lowercase region values
+sumologic_deployment_region=`echo "$sumologic_deployment_region" | awk '{print tolower($0)}'`
+
+case $sumologic_deployment_region  in
+  "us1")
+    sumologic_api_endpoint="https://service.sumologic.com"
+    ;;
+  "us2")
+    sumologic_api_endpoint="https://service.us2.sumologic.com"
+    ;;
+  "jp")
+    sumologic_api_endpoint="https://service.jp.sumologic.com"
+    ;;
+  "in")
+    sumologic_api_endpoint="https://service.in.sumologic.com"
+    ;;
+  "fed")
+    sumologic_api_endpoint="https://service.fed.sumologic.com"
+    ;;
+  "eu")
+    sumologic_api_endpoint="https://service.eu.sumologic.com"
+    ;;
+  "de")
+    sumologic_api_endpoint="https://service.de.sumologic.com"
+    ;;
+  "ca")
+    sumologic_api_endpoint="https://service.ca.sumologic.com"
+    ;;
+  "au")
+    sumologic_api_endpoint="https://service.au.sumologic.com"
+    ;;
+esac
+
+./scripts/update_sdo_variable sumo_api_endpoint $sumologic_api_endpoint
