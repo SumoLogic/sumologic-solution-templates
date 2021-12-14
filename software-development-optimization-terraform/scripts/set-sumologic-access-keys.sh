90a74b4411b2c990a1e8d400720fab07c49a7873
Carl Caum <carl@carlcaum.com>
2021-12-14 10:52:17 -0800

    Use correct API endpoints and set downcased region

--- software-development-optimization-terraform/scripts/set-sumologic-access-keys.sh
+++ software-development-optimization-terraform/scripts/set-sumologic-access-keys.sh
@@ -19,38 +19,37 @@ read -r sumologic_access_key
 echo -n "Sumo Logic Deployment Region: "
 sumologic_deployment_region=""
 read -r sumologic_deployment_region
-./scripts/update_sdo_variable deployment $sumologic_deployment_region
-
 # Make sure we only have to deal with lowercase region values
 sumologic_deployment_region=`echo "$sumologic_deployment_region" | awk '{print tolower($0)}'`
+./scripts/update_sdo_variable deployment $sumologic_deployment_region
 
 case $sumologic_deployment_region  in
   "us1")
-    sumologic_api_endpoint="https://service.sumologic.com"
+    sumologic_api_endpoint="https://api.sumologic.com/api/"
     ;;
   "us2")
-    sumologic_api_endpoint="https://service.us2.sumologic.com"
+    sumologic_api_endpoint="https://api.us2.sumologic.com/api/"
     ;;
   "jp")
-    sumologic_api_endpoint="https://service.jp.sumologic.com"
+    sumologic_api_endpoint="https://api.jp.sumologic.com/api/"
     ;;
   "in")
-    sumologic_api_endpoint="https://service.in.sumologic.com"
+    sumologic_api_endpoint="https://api.in.sumologic.com/api/"
     ;;
   "fed")
-    sumologic_api_endpoint="https://service.fed.sumologic.com"
+    sumologic_api_endpoint="https://api.fed.sumologic.com/api/"
     ;;
   "eu")
-    sumologic_api_endpoint="https://service.eu.sumologic.com"
+    sumologic_api_endpoint="https://api.eu.sumologic.com/api/"
     ;;
   "de")
-    sumologic_api_endpoint="https://service.de.sumologic.com"
+    sumologic_api_endpoint="https://api.de.sumologic.com/api/"
     ;;
   "ca")
-    sumologic_api_endpoint="https://service.ca.sumologic.com"
+    sumologic_api_endpoint="https://api.ca.sumologic.com/api/"
     ;;
   "au")
-    sumologic_api_endpoint="https://service.au.sumologic.com"
+    sumologic_api_endpoint="https://api.au.sumologic.com/api/"
     ;;
 esac
 
