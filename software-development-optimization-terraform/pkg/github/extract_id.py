e9fa5d1671597166671b1136de70a5fb6ca93673
Carl Caum <carl@carlcaum.com>
2021-12-13 22:31:15 -0800

    Remove redundant dev code

--- software-development-optimization-terraform/pkg/github/extract_id.py
+++
@@ -1,7 +0,0 @@
-import json
-data = {}
-input_file = open('sumo_folder.json',)
-inputdata = json.load(input_file)
-data['id'] = inputdata['id']
-with open('sumo_folder_id.json', 'w') as outfile:
-    json.dump(data, outfile)
