614ba28d5d14a2b1cf857f5d77795028a8262894
Jayanta Kashyap <jkashyap@sumologic.com>
2022-02-25 15:55:04 +0530

    updated description and readme

--- aws-observability-terraform/variables.tf
+++ aws-observability-terraform/variables.tf
@@ -78,7 +78,7 @@ variable "sumologic_folder_installation_location" {
 
 variable "sumologic_folder_share_with_org" {
   type        = bool
-  description = "Indicates if AWSO folder should be shared (view access) with entire organization. true to enable; false to disable."
+  description = "Indicates if AWS Observability folder should be shared (view access) with entire organization. true to enable; false to disable."
   default     = true
 
 }
