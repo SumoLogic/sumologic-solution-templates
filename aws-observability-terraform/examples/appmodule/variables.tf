eb60a311f67c50d4426aa8acf78e90c88bf0ced9
Himanshu Sharma <hsharma@sumologic.com>
2022-05-16 20:17:06 +0530

    adding app module example and test framework

---
+++ aws-observability-terraform/examples/appmodule/variables.tf
@@ -0,0 +1,137 @@
+variable "sumologic_environment" {
+  type        = string
+  description = "Enter au, ca, de, eu, jp, us2, in, fed or us1. For more information on Sumo Logic deployments visit https://help.sumologic.com/APIs/General-API-Information/Sumo-Logic-Endpoints-and-Firewall-Security"
+
+  validation {
+    condition = contains([
+      "au",
+      "ca",
+      "de",
+      "eu",
+      "jp",
+      "us1",
+      "us2",
+      "in",
+    "fed"], var.sumologic_environment)
+    error_message = "The value must be one of au, ca, de, eu, jp, us1, us2, in, or fed."
+  }
+}
+
+variable "sumologic_access_id" {
+  type        = string
+  description = "Sumo Logic Access ID. Visit https://help.sumologic.com/Manage/Security/Access-Keys#Create_an_access_key"
+
+  validation {
+    condition     = can(regex("\\w+", var.sumologic_access_id))
+    error_message = "The SumoLogic access ID must contain valid characters."
+  }
+}
+
+variable "sumologic_access_key" {
+  type        = string
+  description = "Sumo Logic Access Key. Visit https://help.sumologic.com/Manage/Security/Access-Keys#Create_an_access_key"
+  #sensitive = true
+
+  validation {
+    condition     = can(regex("\\w+", var.sumologic_access_key))
+    error_message = "The SumoLogic access key must contain valid characters."
+  }
+  
+}
+
+variable "sumologic_organization_id" {
+  type        = string
+  description = <<EOT
+            You can find your org on the Preferences page in the Sumo Logic UI. For more information, see the Preferences Page topic. Your org ID will be used to configure the IAM Role for Sumo Logic AWS Sources."
+            For more details, visit https://help.sumologic.com/01Start-Here/05Customize-Your-Sumo-Logic-Experience/Preferences-Page
+        EOT
+  validation {
+    condition     = can(regex("\\w+", var.sumologic_organization_id))
+    error_message = "The organization ID must contain valid characters."
+  }
+}
+
+variable "aws_account_alias" {
+  type        = string
+  description = <<EOT
+            Provide the Name/Alias for the AWS environment from which you are collecting data. This name will appear in the Sumo Logic Explorer View, metrics, and logs.
+            If you are going to deploy the solution in multiple AWS accounts then this value has to be overidden at main.tf file.
+            Do not include special characters in the alias.
+        EOT
+  validation {
+    condition     = can(regex("[a-z0-9]*", var.aws_account_alias))
+    error_message = "Alias must only contain lowercase letters, number and length less than or equal to 30 characters."
+  }
+}
+
+variable "sumologic_folder_installation_location" {
+  type        = string
+  description = "Indicates where to install the app folder. Enter \"Personal Folder\" for installing in \"Personal\" folder and \"Admin Recommended Folder\" for installing in \"Admin Recommended\" folder."
+  validation {
+    condition = contains([
+      "Personal Folder",
+      "Admin Recommended Folder"], var.sumologic_folder_installation_location)
+    error_message = "The value must be one of \"Personal Folder\" or \"Admin Recommended Folder\"."
+  }
+  default     = "Personal Folder"
+
+}
+
+variable "sumologic_folder_share_with_org" {
+  type        = bool
+  description = "Indicates if AWS Observability folder should be shared (view access) with entire organization. true to enable; false to disable."
+  default     = true
+
+}
+
+variable "sumo_api_endpoint" {
+  type = string
+  validation {
+    condition = contains([
+      "https://api.au.sumologic.com/api/",
+    "https://api.ca.sumologic.com/api/", "https://api.de.sumologic.com/api/", "https://api.eu.sumologic.com/api/", "https://api.fed.sumologic.com/api/", "https://api.in.sumologic.com/api/", "https://api.jp.sumologic.com/api/", "https://api.sumologic.com/api/", "https://api.us2.sumologic.com/api/"], var.sumo_api_endpoint)
+    error_message = "Argument \"sumo_api_endpoint\" must be one of the values specified at https://help.sumologic.com/APIs/General-API-Information/Sumo-Logic-Endpoints-and-Firewall-Security."
+  }
+}
+
+variable "apps_folder" {
+  type        = string
+  description = <<EOT
+            Provide a folder name where all the apps will be installed under the Personal folder of the user whose access keys you have entered.
+            Default value will be: AWS Observability Apps
+        EOT
+  default     = "AWS Observability Apps"
+}
+
+variable "monitors_folder" {
+  type        = string
+  description = <<EOT
+            Provide a folder name where all the monitors will be installed under Monitor Folder.
+            Default value will be: AWS Observability Monitors
+        EOT
+  default     = "AWS Observability Monitors"
+}
+
+variable "alb_monitors" {
+  type        = bool
+  description = "Indicates if the ALB Apps monitors should be enabled. true to disable; false to enable."
+  default     = true
+}
+
+variable "ec2metrics_monitors" {
+  type        = bool
+  description = "Indicates if EC2 Metrics Apps monitors should be enabled. true to disable; false to enable."
+  default     = true
+}
+
+variable "ecs_monitors" {
+  type        = bool
+  description = "Indicates if ECS Apps monitors should be enabled. true to disable; false to enable."
+  default     = true
+}
+
+variable "elasticache_monitors" {
+  type        = bool
+  description = "Indicates if Elasticache Apps monitors should be enabled. true to disable; false to enable."
+  default     = true
+}
---
+++ aws-observability-terraform/test/README.md
@@ -0,0 +1,72 @@
+# Unit/Integration Tests for Sumo Logic SDO Terraform
+
+#### Unit Tests
+
+The unit tests verify/run following:
+1. `terraform` installation.
+2. `tflint` installation.
+3. Run `tflint`.
+4. Run `terraform init`. Make sure third party plugins are installed otherwise this will fail.
+5. Run `terraform validate`.
+6. Run `terraform fmt`.
+
+#### Running unit tests
+
+```shell
+cd test
+sh unit_tests.sh
+```
+
+#### Integration Tests
+
+[Terratest](https://terratest.gruntwork.io/) is being used for Integration testing of the Sumo Logic SDO Terraform scripts.
+
+Following objects are verified:
+
+1. Sumo Logic:
+
+    * Collector.
+    * Sources for each app i.e. Jira Server, Jira Cloud, Opsgenie, Bitbucket, Github and Pagerduty.
+    * App installation for Jira Cloud, Jira Server, Opsgenie, Bitbucket, Github and Pagerduty.
+    * Webhooks for Jira Cloud, Jira Server, Jira Service Desk, Opsgenie and Pagerduty.
+
+2. Atlassian
+
+    * Opsgenie Integration.
+
+3. Pagerduty
+
+    * Pagerduty Integration.
+
+
+#### Running integration tests
+
+
+1. Configure the execution options in `sumologic.auto.tfvars`, `atlassian.auto.tfvars`, `github.auto.tfvars`, `pagerduty.auto.tfvars` and `webhooks.auto-tfvars`.
+2. Configure your Sumo Logic Username by setting the environment variable:
+
+    * `SUMOLOGIC_USERNAME`, it should be the same user with which the access id is created.
+
+3. Install Terraform and make sure it's on your PATH.
+4. Install Golang and make sure it's on your PATH.
+5. Execute Tests
+
+    ```shell
+    cd test
+    dep ensure
+    go test -v -timeout 30m
+    ```
+6. The tests are divided into multiple stages:
+
+    * deploy
+    * validateSumoLogic
+    * validateAtlassian
+    * validatePagerduty
+    * cleanup
+
+    All the stages are executed by default. If you would like to skip a stage set the environment variables like `SKIP_deploy=true`.
+    This is very helpful for example if you are modifying the code and do not want to create/destroy resources with each test run.
+    To achieve this, for the first run you would set `SKIP_cleanup=true` and all other variables should be unset.
+    For the second run it would be `SKIP_cleanup=true` and `SKIP_deploy=true`.
+
+    Now, you can run tests without creating/destroying resources with each run. Once you are finished, unset `SKIP_cleanup` and run the tests to clean up the resources.
