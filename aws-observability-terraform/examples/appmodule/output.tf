a4eb200a02788e703fe8c1b604538078b71d1a56
soagarwal07 <soagarwal@sumologic.com>
2022-06-02 14:57:40 +0530

    remove test cases for rce dashboards

--- aws-observability-terraform/examples/appmodule/output.tf
+++ aws-observability-terraform/examples/appmodule/output.tf
@@ -58,10 +58,10 @@ output "rds_apps_folder_id" {
   description = "This output contains sumologic RDS apps folder."
 }
 
-output "rce_apps_folder_id" {
-  value       = module.sumo-module.sumologic_content_rce.RceApp.id
-  description = "This output contains sumologic Rce apps folder."
-}
+# output "rce_apps_folder_id" {
+#   value       = module.sumo-module.sumologic_content_rce.RceApp.id
+#   description = "This output contains sumologic Rce apps folder."
+# }
 
 output "monitors_folder_id" {
   value       = module.sumo-module.sumologic_monitors_folder.id
