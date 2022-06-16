63b9df37fcfb7879dd4d0c7c735627484616b8b8
soagarwal07 <soagarwal@sumologic.com>
2022-06-16 19:20:34 +0530

    SNS added

--- aws-observability-terraform/app-modules/main.tf
+++ aws-observability-terraform/app-modules/main.tf
@@ -225,6 +225,24 @@ module "elb_app" {
   group_notifications      = var.group_notifications
 }
 
+# Install the sns app and resources.
+module "sns_app" {
+  depends_on = [module.elb_app]
+  source     = "./sns"
+
+  access_id                = var.access_id
+  access_key               = var.access_key
+  environment              = var.environment
+  json_file_directory_path = var.json_file_directory_path
+  app_folder_id            = sumologic_folder.apps_folder.id
+  monitor_folder_id        = sumologic_monitor_folder.monitor_folder.id
+  monitors_disabled        = var.apigateway_monitors_disabled
+  connection_notifications = var.connection_notifications
+  email_notifications      = var.email_notifications
+  group_notifications      = var.group_notifications
+  
+}
+
 # ********************** Create Explore Hierarchy ********************** #
 resource "sumologic_hierarchy" "awso_hierarchy" {
   name = "AWS Observability"
@@ -292,6 +310,12 @@ resource "sumologic_hierarchy" "awso_hierarchy" {
           condition = "AWS/NetworkELB"
           level {
               entity_type = "networkloadbalancer"
+            }
+          }
+        next_levels_with_conditions {
+          condition = "AWS/SNS"
+          level {
+              entity_type = "topicname"
             }
           }
       }
