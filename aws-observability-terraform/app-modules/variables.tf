967d8b72256b694a3063df20bad48ec37413c4f6
soagarwal07 <soagarwal@sumologic.com>
2022-07-18 16:12:35 +0530

    SNS_App

--- aws-observability-terraform/app-modules/variables.tf
+++ aws-observability-terraform/app-modules/variables.tf
@@ -49,7 +49,7 @@ variable "apps_folder_name" {
             Provide a folder name where all the apps will be installed under the Personal folder of the user whose access keys you have entered.
             Default value will be: AWS Observability Apps
         EOT
-  default     = "AWS Observability Apps"
+  default     = "AWS Observability Apps TF"
 }
 
 variable "parent_folder_id" {
@@ -64,7 +64,7 @@ variable "monitors_folder_name" {
             Provide a folder name where all the monitors will be installed under Monitor Folder.
             Default value will be: AWS Observability Monitors
         EOT
-  default     = "AWS Observability Monitors"
+  default     = "AWS Observability Monitors TF"
 }
 
 variable "connection_notifications" {
