aa0fd9c8032e0e825be5d63987ce79169bc68305
Himanshu Sharma <hsharma@sumologic.com>
2023-03-20 14:39:04 +0530

    Restoring db_component.tf

--- application-components/db_component.tf
+++ application-components/db_component.tf
@@ -61,16 +61,16 @@ resource "sumologic_field" "pod_labels_db_cluster_port" {
 
 # ********************** Database component FERs ********************** #
 
-# resource "sumologic_field_extraction_rule" "SumoLogicFieldExtractionRulesForDatabase" {
-#   depends_on       = [sumologic_field.db_cluster, sumologic_field.db_system, sumologic_field.db_cluster_address, sumologic_field.db_cluster_port, sumologic_field.pod_labels_db_cluster, sumologic_field.pod_labels_db_system, sumologic_field.pod_labels_db_cluster_address, sumologic_field.pod_labels_db_cluster_port]
-#   count            = length(local.all_components_values) > 0 && local.has_any_kubernetes_deployments ? 1 : 0
-#   enabled          = true
-#   name             = local.database_fer_name
-#   parse_expression = <<EOT
-#         if (!isEmpty(pod_labels_environment), pod_labels_environment, "") as environment
-#         | pod_labels_component as component
-#         | pod_labels_db_system as db_system
-#         | pod_labels_db_cluster as db_cluster
-#     EOT
-#   scope            = "account=prod"
-# }
+resource "sumologic_field_extraction_rule" "SumoLogicFieldExtractionRulesForDatabase" {
+  depends_on       = [sumologic_field.db_cluster, sumologic_field.db_system, sumologic_field.db_cluster_address, sumologic_field.db_cluster_port, sumologic_field.pod_labels_db_cluster, sumologic_field.pod_labels_db_system, sumologic_field.pod_labels_db_cluster_address, sumologic_field.pod_labels_db_cluster_port]
+  count            = length(local.all_components_values) > 0 && local.has_any_kubernetes_deployments ? 1 : 0
+  enabled          = true
+  name             = local.database_fer_name
+  parse_expression = <<EOT
+        if (!isEmpty(pod_labels_environment), pod_labels_environment, "") as environment
+        | pod_labels_component as component
+        | pod_labels_db_system as db_system
+        | pod_labels_db_cluster as db_cluster
+    EOT
+  scope            = "pod_labels_environment=* pod_labels_component=database pod_labels_db_system=* pod_labels_db_cluster=*"
+}
