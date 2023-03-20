8611688834acbe71e1695faa7ce7d0ef7c5300ee
Himanshu Sharma <hsharma@sumologic.com>
2023-03-20 14:36:55 +0530

    Updating app components monitors with FGP block

--- application-components/mongodb_monitors.tf
+++ application-components/mongodb_monitors.tf
@@ -4,6 +4,11 @@ resource "sumologic_monitor_folder" "mongodb_monitor_folder" {
   name        = var.mongodb_monitor_folder
   description = "Folder for MongoDB Monitors"
   parent_id   = sumologic_monitor_folder.root_monitor_folder.id
+  obj_permission {
+    subject_type = "org"
+    subject_id = var.sumologic_organization_id
+    permissions = ["Create", "Read", "Update", "Delete", "Manage"]
+  }
 }
 
 module "MongoDB-TooManyCursorsTimeouts" {
