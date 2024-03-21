40988b1d30280a0f5ac3124b8e05f7bbc85bf8c2
Himanshu Pal <hp.iiita@gmail.com>
2024-03-21 11:49:36 +0530

    added sync.sh and internal readme files

---
+++ aws-observability/INTERNAL_README.md
@@ -0,0 +1,7 @@
+## Upload artifacts
+
+1. Make sure aws cli is configured locally
+2. Update the BUCKET, BUCKET_REGION, AWS_PROFILE 
+3. Verify the files which will be uploaded / updated with the dry run flag and then run the same command without the flag.
+   `sh sync.sh --dryrun` 
+   `sh sync.sh`
---
+++ aws-observability/sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Source directory for CloudFormation (CF) templates
+MASTER_TEMPLATE=./templates/sumologic_observability.master.template.yaml
+APPS_TEMPLATES_DIR=./apps
+APPS_JSON_DIR=./json
+
+# Bucket name
+CF_VERSION=v2.8.0
+BUCKET_REGION=us-east-1
+BUCKET=sumologic-appdev-aws-sam-apps
+S3_KEY_PREFIX=aws-observability-versions/${CF_VERSION}
+
+# S3 Location
+S3_TEMPLATES=s3://${BUCKET}/${S3_KEY_PREFIX}/
+S3_JSON_DIR=s3://${BUCKET}/${S3_KEY_PREFIX}/appjson
+
+# Sync options (Add --exclude '*.zip' if you don't want to upload lambda)
+AWS_PROFILE=prod
+
+if [[ " $@ " =~ " --dryrun " ]]; then
+    isdryrun="--dryrun"
+else
+    isdryrun=""
+fi
+
+echo "Copying child templates from ${APPS_TEMPLATES_DIR} to Bucket ${S3_TEMPLATES} in ${BUCKET_REGION}"
+aws s3 sync ${APPS_TEMPLATES_DIR}/ ${S3_TEMPLATES} --delete --include '*.yaml' --exclude '*.DS_Store'  --exclude '*/test/*' --exclude 'SumoLogicAWSObservabilityHelper/*' --acl public-read ${isdryrun} --profile ${AWS_PROFILE} --region ${BUCKET_REGION}
+echo "Copying json folder ${APPS_JSON_DIR} to Bucket ${S3_JSON_DIR} in ${BUCKET_REGION}"
+aws s3 cp --recursive ${APPS_JSON_DIR} ${S3_JSON_DIR} ${isdryrun} --profile ${AWS_PROFILE} --region ${BUCKET_REGION}
+echo "Copying master template ${MASTER_TEMPLATE} to Bucket ${S3_TEMPLATES} in ${BUCKET_REGION}"
+aws s3 cp ${MASTER_TEMPLATE} ${S3_TEMPLATES} ${isdryrun} --profile ${AWS_PROFILE} --region ${BUCKET_REGION}
