59d67b06448fa4e2efcbeeb634981aeff5b4cf7c
Akhil Dangore <akhil.dangore.ctr@sumologic.com>
2024-06-06 17:15:18 +0530

    Upgrade go to 1.22.0 and it's dependencies

--- aws-observability-terraform/examples/sourcemodule/overrideSources/versions.tf
+++ aws-observability-terraform/examples/sourcemodule/overrideSources/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.42.0, < 4.0.0"
+      version = ">= 5.16.2, < 6.0.0"
     }
     sumologic = {
       version = ">= 2.28.3, < 3.0.0"
