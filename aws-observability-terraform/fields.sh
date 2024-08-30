6fe3169ca29a090597687baeb5073a0dc52704db
Himanshu Sharma <hsharma@sumologic.com>
2024-08-30 12:59:36 +0530

    Updating fields.sh script to support korea

--- aws-observability-terraform/fields.sh
+++ aws-observability-terraform/fields.sh
@@ -10,8 +10,8 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Validate Sumo Logic environment/deployment.
-if ! [[ "$SUMOLOGIC_ENV" =~ ^(au|ca|de|eu|jp|us2|in|fed|us1)$ ]]; then
-    echo "$SUMOLOGIC_ENV is invalid Sumo Logic deployment. For SUMOLOGIC_ENV, provide one from list : au, ca, de, eu, jp, us2, in, fed or us1. For more information on Sumo Logic deployments visit https://help.sumologic.com/APIs/General-API-Information/Sumo-Logic-Endpoints-and-Firewall-Security"
+if ! [[ "$SUMOLOGIC_ENV" =~ ^(au|ca|de|eu|jp|us2|in|fed|us1|kr)$ ]]; then
+    echo "$SUMOLOGIC_ENV is invalid Sumo Logic deployment. For SUMOLOGIC_ENV, provide one from list : au, ca, de, eu, fed, in, jp, kr, us1 or us2. For more information on Sumo Logic deployments visit https://help.sumologic.com/APIs/General-API-Information/Sumo-Logic-Endpoints-and-Firewall-Security"
     exit 1
 fi
 
