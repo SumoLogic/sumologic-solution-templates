6af9356d109e4c93813c38a67699749077bb8825
Akhil Dangore <akhil.dangore.ctr@sumologic.com>
2026-04-20 18:24:54 +0530

    Updated Runtime and SAM version

--- aws-observability-terraform/source-module/main.tf
+++ aws-observability-terraform/source-module/main.tf
@@ -103,7 +103,7 @@ module "elb_module" {
   }
 
   auto_enable_access_logs = var.auto_enable_access_logs
-  app_semantic_version    = "1.0.18"
+  app_semantic_version    = "1.0.19"
   auto_enable_access_logs_options = {
     filter                 = "'Type': 'application'|'type': 'application'"
     remove_on_delete_stack = true
@@ -150,7 +150,7 @@ module "classic_lb_module" {
     }
   }
   auto_enable_access_logs = var.auto_enable_classic_lb_access_logs
-  app_semantic_version    = "1.0.18"
+  app_semantic_version    = "1.0.19"
   auto_enable_access_logs_options = {
     bucket_prefix          = local.auto_classic_lb_path_exp
     auto_enable_logging    = "ELB"
