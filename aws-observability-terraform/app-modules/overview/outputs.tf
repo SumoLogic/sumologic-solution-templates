3eec562b00aa33e76a1db490073d0612206768bb
Akhil Dangore <akhil.dangore.ctr@sumologic.com>
2026-07-22 09:55:56 +0530

    Removed old aws-observability-terraform code

--- aws-observability-terraform/app-modules/overview/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sumologic_field" {
-  value       = module.overview_module.sumologic_field
-  description = "This output contains fields required for overview app."
-}
-
-output "sumologic_content" {
-  value       = module.overview_module.sumologic_content
-  description = "This output contains overview App."
-}
--- aws-observability-terraform/cloudformation-module/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cloudformation_stack_id" {
-  value = aws_cloudformation_stack.aws-observability.id
-}
